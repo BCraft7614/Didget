e81b24333c1a0852836c32a258400c97404f1cee
rtran <rtran@PRG22731>
2016-11-28 13:52:26 -0500

    Fuck this shit

---
+++ BPA--Game/BPA--Game/Entity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace BPA__Game
+{
+    class Entity
+    {
+        public virtual void UnloadContent()
+        {
+            UnloadContent();
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+        public virtual void Initialize()
+        {
+
+        }
+    }
+}
